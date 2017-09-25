4775dd5c6da48d07b4a3feb9f095b48c3e7a0f72
mkchang <martinkchang@gmail.com>
2017-09-25 14:57:35 -0700

    Setup intial files and add pseudocode for plan

---
+++ index.js
@@ -0,0 +1,16 @@
+var inquirer = require('inquirer');
+var game = require('./tictactoe');
+
+// Start of game
+// Make a new board
+var board = new game.Board();
+
+// Prompt player 1 for a move
+  // execute move
+  // show board
+  // check for win
+// Prompt Player 2 for a move
+  // execute move
+  // show board
+  // check for win
+// repeat above
